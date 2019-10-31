5fd0654f42bbf2b10b81c6e58b321854d5d71112
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-01 01:35:35 +0200

    added functions for the login and main views.

--- blog.py
+++ blog.py
@@ -14,5 +14,13 @@ def connect_db():
     """Function for connecting to the database"""
     return sqlite3.connect(app.config['DATABASE'])
 
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
