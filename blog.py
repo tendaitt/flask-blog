5040a692004dd152bb88bae46652b6b5895a7791
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-01 17:35:40 +0200

    add login_required decorator to restrict access to main.html.

--- blog.py
+++ blog.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 DATABASE = 'blog.db'
 
@@ -18,6 +19,16 @@ app.config.from_object(__name__)
 def connect_db():
     """Function for connecting to the database"""
     return sqlite3.connect(app.config['DATABASE'])
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first.')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -42,6 +53,7 @@ def logout():
     return redirect(url_for('login'))
 
 @app.route('/main')
+@login_required
 def main():
     return render_template('main.html')
 
